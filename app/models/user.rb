997fd666e38dddd1597deae5818d2d766bfaad38
KBENBENECK <kbenbeneck@gmail.com>
2020-04-05 17:13:38 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
     has_many :meals
     validates :email, uniqueness: true
     validates :email, presence: true
+    validates :name, uniqueness: true
+    validates :name, presence: true
 end
