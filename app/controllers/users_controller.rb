d2c98f80adfae49bf8a6e7a18fdc1de6c1633fe6
KBENBENECK <kbenbeneck@gmail.com>
2020-04-23 16:45:12 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,19 @@
 class UsersController < ApplicationController
     get '/signup' do
+        @user = User.new
         erb :"users/new"
     end
 
     post '/users' do
-        
-    #     user = User.create(name: params[:name], password: params[:password], email: params[:email])
-
-    #     if user.save
-            
-    #         redirect '/login'
-    #     else
-    #         redirect '/login'
-    #     end
-    # end
-
-
-
-    
         @user = User.new
         @user.name = params[:name]
         @user.email = params[:email]
         @user.password = params[:password]
         if @user.save
-            redirect '/login'
+            session[:user_id] = @user.id
+            redirect '/meals'
         else
-            redirect '/signup'
+            erb :"users/new"
         end
     end
 end
